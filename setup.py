a3314494e68160cb01b0bde5b23ab49d3f6c0795
tomotaka-yamasaki <tomotaka.yamasaki@aktsk.jp>
2022-06-20 13:32:26 +0900

    feat: CLIにするため、スクリプトをまとめていたリポジトリから分離

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='today-google-calendar',
+    description="GoogleCalendarから一日の予定を取得し、整形して表示する",
+    version='1.0.0',
+    install_requires=['httplib2', 'google-api-python-client', 'oauth2client'],
+    packages=['src', 'src.google', 'src.config', 'src.credentials'],
+    entry_points={
+        'console_scripts': [
+            'today-google-calendar=src.today_google_calendar:main',
+        ],
+    }
+)
