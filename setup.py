587d7a569dfe395b215044300c184aefe36f5d4c
tomotaka-yamasaki <tomotaka.yamasaki@aktsk.jp>
2023-01-19 14:17:43 +0900

    feat: コマンドラインオプションをclickで実装し直す

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='today-google-calendar',
     description="GoogleCalendarから一日の予定を取得し、整形して表示する",
-    version='1.0.0',
-    install_requires=['httplib2', 'google-api-python-client', 'oauth2client'],
+    version='1.1.0',
+    install_requires=['httplib2', 'google-api-python-client', 'oauth2client', 'click'],
     packages=['src', 'src.google', 'src.config', 'src.credentials'],
     entry_points={
         'console_scripts': [
--- src/today_google_calendar.py
+++ src/today_google_calendar.py
@@ -3,32 +3,29 @@ from src.config import config
 import os.path
 import datetime
 from datetime import timedelta
-import argparse
+import click
 
 def get_calendar(calendar_id):
     cred_dir = os.path.join(os.path.dirname(__file__), config.SA_KEY_PATH)
     return calendar.GoogleCalendar(calendar_id, cred_dir)
 
-def output_oneday_calendar(calendar_id, dase_date):
-    calendar = get_calendar(calendar_id)
-    schedules = calendar.get_schedules(dase_date - timedelta(days=1), dase_date)
 
-    print('## 【スケジュール】本日の予定')
-    for schedule in schedules:
-        print(schedule.output())
+@click.command()
+@click.option('--calendar-id', '-i', type=str, default=config.CALENDAR_ID, help='取得したいGoogleカレンダーID')
+@click.option('--date', '-d', type=click.DateTime(['%Y-%m-%d', '%Y/%m/%d']), default=datetime.datetime.today().strftime('%Y-%m-%d'), help='予定の取得日 (yyyy-mm-dd or yyyy/mm/dd) default:Today')
+def output_oneday_calendar(calendar_id, date):
+    calendar = get_calendar(calendar_id)
+    schedules = calendar.get_schedules(date - timedelta(days=1), date)
+
+    schedule_texts = [schedule.output() for schedule in schedules]
+    output_texts = ['## 【スケジュール】本日の予定']
+    output_texts.extend(schedule_texts)
+    output_text = '\n'.join(output_texts)
+
+    print(output_text)
 
 def main():
-    # カレンダー取得範囲のデフォルト
-    today = datetime.datetime.today().strftime('%Y-%m-%d')
-    date_type = lambda s: datetime.datetime.strptime(s, '%Y-%m-%d')
-
-    # コマンドライン引数設定
-    parser = argparse.ArgumentParser(description='Googleカレンダーから予定を取得する')
-    parser.add_argument('-i', '--calendarId', default=config.CALENDAR_ID, help='取得したいGoogleカレンダーID')
-    parser.add_argument('-d', '--today', type=date_type, default=today, help='予定の取得日 yyyy-mm-dd default:Today')
-
-    args = parser.parse_args()
-    output_oneday_calendar(args.calendarId, args.today)
+    output_oneday_calendar()
 
 if __name__ == '__main__':
     main()
